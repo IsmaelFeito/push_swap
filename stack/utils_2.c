0d464bd5c1c83acc0e42ae4cf972e38476e507ed
Ismael <isfemo15@gmail.com>
2024-09-15 11:50:32 +0200

    4

--- stack/utils_2.c
+++ stack/utils_2.c
@@ -6,7 +6,7 @@
 /*   By: ifeito-m <ifeito-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 18:06:33 by ifeito-m          #+#    #+#             */
-/*   Updated: 2024/09/14 21:13:41 by ifeito-m         ###   ########.fr       */
+/*   Updated: 2024/09/15 11:47:20 by ifeito-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,56 @@ void	free_str(char **content)
 	while (*content[i])
 		free(*content[i++]);
 	free(**content);
+}
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t					i;
+	unsigned char			*u1;
+	const unsigned char		*u2;
+
+	if(!s1 )
+	u1 = (unsigned char *)s1;
+	u2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (u1[i] - u2[i])
+			return (u1[i] - u2[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	**ordered_array(char	**content)
+{
+	char	**order;
+	int		i = 0;
+	int		j = 0;
+	
+	order = malloc(pointer_size(content) * sizeof(char *));
+	if(!order)
+		return (NULL);
+	while(content[i])
+		order[i] = content[i++];
+	i = 0;
+	while(i < (pointer_size(order) - 1))//i < tamano ptr - '\0'
+	{
+		j = i + 1;
+		while(j < pointer_size(order))
+		{
+			if(ft_memcmp(content[i++], order[j], 1) < 0)
+				ft_memcpy(order[j], content[i], sizeof(char *));
+				if(i == pointer_size(order) - 1)
+				{
+					i = 0;
+					j++;
+					if((ft_memcmp(content[i++], order[j], 1) < 0 && j < pointer_size(order)))
+						
+				}
+		}
+		j++;
+		i++;
+	}
+	return(order);
 }
