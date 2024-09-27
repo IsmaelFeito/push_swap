f6af62d5d82872eeda7843d09196774db98e792e
Ismael Feito Moreno <ifeito-m@c3r2s3.42madrid.com>
2024-09-27 04:53:06 +0200

    imprimir lista

--- main.c
+++ main.c
@@ -13,27 +13,3 @@ int main (int argc, char **argv)
 	free_list(stack_b);
     return 0;
 }
-
-// int	main(int ac, char *av[])
-// {
-// 	t_stack	**stack_a;
-// 	t_stack	**stack_b;
-
-	
-// 	if (ac > 1)
-// 	{
-// 		stack_creator(av, stack_a);
-// 		if (!stack_sorted(*stack_a))
-// 		{
-// 			if (stack_len(*stack_a) == 2)
-// 				swap(stack_a, stack_b, MOVESA);
-// 			else if (stack_len(*stack_a) == 3)
-// 				sort_stack_three(stack_a);
-// 			else
-// 				sort_stack(stack_a, stack_b);
-// 		}
-		
-// 	}
-// 	else
-// 		ft_putstr_fd("Error\n", STDERR_FILENO);
-// }
